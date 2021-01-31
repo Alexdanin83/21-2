ea2fc98f68867eddd23c7e1d93d899293a8eb1ee
Alex Danin <alexey.danin@gmail.com>
2021-01-31 17:45:04 +0100

    Modul_21-3_hbs

--- server.js
+++ server.js
@@ -1,32 +1,41 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
-app.use(express.static(path.join(__dirname, '/views/img')));
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-app.use('/user/', (req, res, next) => {
-  res.show('forbiden.html');
+app.use(express.static(path.join(__dirname, '/public')));
 
-});
 app.get('/', (req, res) => {
-  res.show('home.html');
+  res.render('index');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', {name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' });
 });
 
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
 
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
 
 app.use((req, res) => {
-if (res.status(404)) {res.show('404.html');}
+  res.status(404).send('404 not found...');
 })
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
